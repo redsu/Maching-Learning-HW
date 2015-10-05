849f22a3d0049adf0509c6bf4abeff0b143e63d1
redsuredsu <b00611018@ntu.edu.tw>
2015-10-05 10:27:42 +0800

    Q16 is ok

--- ML_hw1.c
+++ ML_hw1.c
@@ -1,5 +1,6 @@
 // vim: ts=4
 #include<stdio.h>
+#include<time.h>
 #define false 0
 #define true  1
 double dot(double data1[], double data2[]){
@@ -22,6 +23,7 @@ int main(){
 	fin = fopen("hw1_train.dat.txt", "r");
 	int n = 0, i = 0;
 	double data[410][10];
+	srand(time(NULL));
 	for(i=0; i<400; i++){
 		fscanf(fin, "%lf %lf %lf %lf %lf", 
 			   &data[i][0],&data[i][1],&data[i][2],&data[i][3],&data[i][5]);
@@ -35,6 +37,22 @@ int main(){
 		Wt[i] = data[0][i];
 	Wt[4] = 1;
 	i=0;
+	int list[410], choose[410];
+	for(i=0;i<400;i++)
+		list[i] = choose[i] = -1;
+	int index = 0;
+
+	for(i=0; i<400;){
+		index = rand()%400;
+		if(choose[index]==-1){
+			list[i] = index;
+			choose[index] = 1;
+			i++;
+		}
+	}
+	for(i=0; i<4; i++)
+        Wt[i] = data[list[0]][i];
+	Wt[4] = data[list[0]][4];
 	int j = 0, times = 1;
 	//char end = false;
 	double result = 0;
@@ -45,37 +63,37 @@ int main(){
 		//printf("round : %d\n", round);
 		//times = 1;
 		for(i=0; i<400; i++){
-			if(sign)
-				sign = 0;
-			result = dot(Wt, data[i]);
+//			if(sign)
+//				sign = 0;
+			result = dot(Wt, data[list[i]]);
 			//sign = result > 0 ? 1 : -1;
-			if(result * data[i][5] <= 0){
-				//printf("update! %d Node: %d\n", times, i);
+			if(result * data[list[i]][5] <= 0){
+//				printf("update! %d Node: %d\n", times, i);
 				for(j=0; j<5; j++){
-					Wt[j]+=data[i][j]*data[i][5];
-					//printf("%lf ", Wt[j]);
+					Wt[j]+=data[list[i]][j]*data[list[i]][5];
+//					printf("%lf ", Wt[j]);
 				}
-				//printf("\n");
+//				printf("\n");
 				times++;
 				ok = 0;
 				//break;
 			}
 			else{
-				//printf("correct!\n");
+//				printf("correct!\n");
 				ok++;
 			}
 			if(ok==400)
 				break;
 		}
-		//times++;
 		if(ok==400)
 			break;
 		//printf("%d time(s).\n", i);
 		round ++;
 	}
-	printf("%d time(s).\n", times);
-	for(i=0; i<5; i++)
+	printf("%d\n", times);
+	//printf("%d time(s).\n", times);
+/*	for(i=0; i<5; i++)
 		printf("%lf ", Wt[i]);
 	printf("\n");
-	return 0;
+*/	return 0;
 }
